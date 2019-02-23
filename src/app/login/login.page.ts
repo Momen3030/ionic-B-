a7ceb6152320ec6749f8c98282022dfb4242f35d
Momen3030 <momenadel3030@gmail.com>
2019-02-23 22:33:10 +0200

    working in login form

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RestApiService } from '../services/rest-api.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-
-  constructor() { }
-
+  users:any;
+  username: string ='';
+  password: string ='';
+  constructor(private _UserService:RestApiService ) { }
+ 
   ngOnInit() {
+
+    // this._UserService.apiData.subscribe(
+    //   (users) => { this.users = users.data;
+    //       console.log(this.users); });
   }
+  
+
+
+  login() {
+      
+
+       console.log(this.username);
+    
+        // for (let i = 0; i < this.users.length; i++) {
+           
+        //       if (this.users[i].email === this.form.value.email
+        //          && this.users[i].userpass === this.form.value.password){
+        //         this.myRoute.navigate(['cartproducts']); 
+        //         } 
+        //     else {
+        //         this.myRoute.navigate(['home']);
+        //     }
+        
+    }
+
+
+
+
 
 }
